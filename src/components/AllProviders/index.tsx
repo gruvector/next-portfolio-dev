e0bb834526a225867c97fcf91e20b0531d0ed403
gruvector <148240965+gruvector@users.noreply.github.com>
2024-02-10 02:50:54 +0900

    Update index.tsx

--- src/components/AllProviders/index.tsx
+++ src/components/AllProviders/index.tsx
@@ -3,10 +3,13 @@
 import { ThemeProvider } from 'next-themes';
 import { ReactNode } from 'react';
 
+
 type AllProvidersProps = {
   children: ReactNode;
 };
 
+
 const AllProviders = ({ children }: AllProvidersProps) => <ThemeProvider attribute="class">{children}</ThemeProvider>;
+
 
 export default AllProviders;
