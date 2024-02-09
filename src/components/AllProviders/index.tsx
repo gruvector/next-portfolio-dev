8292226bdb6352fb9c132eb9a9d32f23075d9913
gruvector <vector.gru316@gmail.com>
2024-02-10 02:57:58 +0900

    correction providers

--- src/components/AllProviders/index.tsx
+++ src/components/AllProviders/index.tsx
@@ -7,6 +7,6 @@ type AllProvidersProps = {
   children: ReactNode;
 };
 
-const AllProviders = ({ children }: AllProvidersProps) => <ThemeProvider attribute="class">{children}</ThemeProvider>;
+const AllProviders = ({ children }: AllProvidersProps) => <ThemeProvider attribute = "class"> {children} </ThemeProvider>;
 
 export default AllProviders;
